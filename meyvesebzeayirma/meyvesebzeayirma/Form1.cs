bb01fcab9ab1e9100320c6755da767c083827d95
elifftorun <145452036+elifftorun@users.noreply.github.com>
2023-10-23 15:48:58 +0300

    Add files via upload

---
+++ meyvesebzeayirma/meyvesebzeayirma/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace meyvesebzeayirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAyır_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < lstTumu.Items.Count; i++)
+            {
+                if (lstTumu.Items[i].ToString()=="Domates"||
+                    lstTumu.Items[i].ToString()=="Patates"||
+                    lstTumu.Items[i].ToString()=="Biber"||
+                    lstTumu.Items[i].ToString() == "Patlıcan" ||
+                    lstTumu.Items[i].ToString() == "Soğan" ||
+                    lstTumu.Items[i].ToString() == "Lahana" ||
+                    lstTumu.Items[i].ToString() == "Brokoli" ||
+                    lstTumu.Items[i].ToString() == "Havuç")
+                {
+                    lstSebze.Items.Add(lstTumu.Items[i]);
+                }
+                else
+                {
+                    lstMeyve.Items.Add(lstTumu.Items[i]);
+                }
+            }
+        }
+    }
+}
